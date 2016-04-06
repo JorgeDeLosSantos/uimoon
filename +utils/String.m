638e2a776dd5dd58323c7ed5ea4e41053cd07bb8
Jorge De Los Santos <delossantosmfq@gmail.com>
2016-04-06 15:53:10 -0500

    Utils module added

--- +utils/String.m
+++ +utils/String.m
@@ -1,22 +1,27 @@
 classdef String < handle
     
     properties
+        str_;
     end
     
-    methods (Static)
+    methods
+        function obj = String(str)
+            obj.str_ = str;
+        end
+    end
+    
+    methods (Static=true)
         function s = Split(cad,delimiter)
-            % "Partición" de una cadena
+            % Split string
             if nargin==1
-                % Usar espacio como delimitador en caso
-                % de no especificarse de forma explícita.
+                % Default delimiter -> Space
                 delimiter=' ';
             end
             if isnumeric(cad) % Deprecated
-                % Hacer la conversión si se tiene una entrada numérica
+                % Convert numerical inputs to string
                 cad=num2str(cad);
             end
-			
-			s = regexp(cad,['\',delimiter],'split'); % (0.1.1) --> Rev --> Sust. while loop
+            s = regexp(cad,['\',delimiter],'split'); % (0.1.1) --> Rev --> Sust. while loop
         end
         
         function s = ToLower(str)
@@ -28,7 +33,6 @@ classdef String < handle
             % Upper case
             s = upper(str);
         end
-        
     end
     
 end
