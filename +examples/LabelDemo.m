058d7bab0ff89558f809a17293b1040c07368e8a
Pedro Jorge De Los Santos <delossantosmfq@gmail.com>
2016-04-12 22:15:26 -0500

    Examples updated

--- +examples/LabelDemo.m
+++ +examples/LabelDemo.m
@@ -1,4 +1,5 @@
-function LabelDemo% Demo for UIMOON 0.1.0
+function LabelDemo
+% Demo for UIMOON 0.1.0
 % Author: Pedro Jorge De Los Santos
 % Project URL: https://github.com/JorgeDeLosSantos/uimoon
 % License: MIT License
@@ -11,6 +12,8 @@ app.SetLayout('horizontal',2);
 lab1 = Label('One Label');
 lab2 = Label('Other label');
 lab1.SetColor(Color.RED);
+lab2.SetColor('#00fafa');
+lab2.SetFont(Font('Arial',14,'bold'));
 app.Add(lab1);
 app.Add(lab2);
 end
--- +examples/LayoutDemo.m
+++ +examples/LayoutDemo.m
@@ -1,13 +1,17 @@
-function LayoutDemo
+% function LayoutDemo
 import uimoon.core.*
+import uimoon.utils.*
 
 app = Frame('Layout Demo');
 app.SetSize(400,300);
+app.SetLayout('horizontal'); % 3 rows, 3 columns
 
 p1 = Panel();
 p2 = Panel();
-app.Add(p1);
-app.Add(p2);
+p1.SetLayout('g',2,2,2);
+p2.SetLayout('v');
 
-end
+app.AddMany(p1,p2);
 
+p1.AddMany(Button('1'),Button('2'),Button('3'),Button('4'));
+p2.AddMany(Label('Label 01'),Label('Label 02'),Label('Label 03'),Label('Label 04'));
