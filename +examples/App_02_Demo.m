d02cc01f0a21060288b1d1a2e5f1388415ee4f19
Pedro Jorge De Los Santos <delossantosmfq@gmail.com>
2016-04-16 00:50:05 -0500

    MiniApp Demo

---
+++ +examples/App_02_Demo.m
@@ -0,0 +1,14 @@
+function App_02_Demo
+% Demo for UIMOON 0.1.0
+% Author: Pedro Jorge De Los Santos
+% Project URL: https://github.com/JorgeDeLosSantos/uimoon
+% License: MIT License
+%
+import uimoon.core.*
+
+app = Frame('App',[300,130]);
+app.Center();
+
+
+
+end
