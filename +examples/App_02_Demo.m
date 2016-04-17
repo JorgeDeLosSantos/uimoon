bdcd99fdbf0fa0b4a2e8435066132b3478c55924
Pedro Jorge De Los Santos <delossantosmfq@gmail.com>
2016-04-17 09:29:18 -0500

    EXample > MiniApp 02

--- +examples/App_02_Demo.m
+++ +examples/App_02_Demo.m
@@ -6,9 +6,43 @@ function App_02_Demo
 %
 import uimoon.core.*
 
-app = Frame('App',[300,130]);
+app = Frame('App',[400,300]);
 app.Center();
+app.SetLayout('v',2);
 
+% Axes
+ax = Axes();
+ax.Plot2D(NaN,NaN);
+app.Add(ax,0.9);
 
+% Controls
+pctrl = Panel();
+pctrl.SetLayout('h',1);
+app.Add(pctrl,0.1);
 
+lbl = Label('f(x)');
+fun = TextField();
+chk = CheckBox('Grid',false);
+bt = Button('Plot');
+
+% Add controls to Panel
+pctrl.Add(lbl,0.1);
+pctrl.Add(fun,0.5);
+pctrl.Add(chk,0.125);
+pctrl.Add(bt,0.275);
+
+% Set button callback
+bt.SetCallback(@plotting);
+chk.SetCallback(@setgrid);
+
+    function plotting(varargin)
+        fx = fun.GetText();
+        x = linspace(0,10,1000);
+        y = eval(fx);
+        ax.Plot2D(x,y);
+    end
+
+    function setgrid(~,~,obj)
+        ax.SetGrid(obj.GetValue());
+    end
 end
